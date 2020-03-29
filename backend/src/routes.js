3b290fe0fb38366ffa17725ff99a157f9b7152f8
leticiams <leticiams_1@hotmail.com>
2020-03-29 18:38:44 -0300

    creating sessioncontroller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
@@ -39,13 +40,15 @@ const routes = express.Router();
     * ps: Knexjs é o quey builder mais utilizado para nodejs
     */
 
-   routes.get('/ongs', OngController.index);
-   routes.post('/ongs', OngController.create);
+    routes.post('/sessions', SessionController.create);
 
-   routes.get('/profile', ProfileController.index);
+    routes.get('/ongs', OngController.index);
+    routes.post('/ongs', OngController.create);
 
-   routes.get('/incidents', IncidentController.index);
-   routes.post('/incidents', IncidentController.create);
-   routes.delete('/incidents/:id', IncidentController.delete);
+    routes.get('/profile', ProfileController.index);
+
+    routes.get('/incidents', IncidentController.index);
+    routes.post('/incidents', IncidentController.create);
+    routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
