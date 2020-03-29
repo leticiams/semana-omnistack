28a82de3d4e6dd09178b8e4ed9f33e34412ef4be
leticiams <leticiams_1@hotmail.com>
2020-03-29 18:53:01 -0300

    adding cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
  * Transforma a requisição feita em JSON no Body Request para Javascript
  */
 
+ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
