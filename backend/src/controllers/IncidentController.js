8b0acf76735212878c39614aa7acda83b4afef7e
leticiams <leticiams_1@hotmail.com>
2020-03-29 18:46:53 -0300

    counting total number of incidents

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -4,10 +4,15 @@ module.exports = {
     async index(request, response) {
         const { page = 1 } = request.query;
 
+        const [count] = await connection('incidents').count();
+
+
         const incidents = await connection('incidents')
           .limit(5)
           .offset((page - 1) * 5)
           .select('*');
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
