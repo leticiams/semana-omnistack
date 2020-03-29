2a2938f977daecc8c4b612c742209529eb158c65
leticiams <leticiams_1@hotmail.com>
2020-03-29 19:36:16 -0300

    css resets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import Header from './Header';
+import './global.css';
+
+import Logon from './pages/Logon';
 
 // Um componente do React é uma função que retorna HTML
 // JSX (JavaScript XML), quando o HTML tá integrado dentro do JavaScript
 
 function App() {
   return (
-    <Header>
-      Semana Omnistack
-    </Header>
+    <Logon />
   );
 }
 
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function Logon
+() {
+    return (
+        <h1>Hello World</h1>
+    );
+}
