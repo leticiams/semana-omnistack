1203999e11494ed40be7d06cc363f01492f1230e
leticiams <leticiams_1@hotmail.com>
2020-03-29 18:42:47 -0300

    limiting number of incidents per page

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,12 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(request, response) {
-        const incidents = await connection('incidents').select('*');
+        const { page = 1 } = request.query;
+
+        const incidents = await connection('incidents')
+          .limit(5)
+          .offset((page - 1) * 5)
+          .select('*');
 
         return response.json(incidents);
     },
