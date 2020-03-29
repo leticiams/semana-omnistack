1d9b08faf1df6688e5bc338607bd9165d1b66cad
leticiams <leticiams_1@hotmail.com>
2020-03-29 15:52:22 -0300

    tracking params

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parâmetros: 
+  * 
+  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtors, paginação)
+  * Route Params: Parâmetros utilizados para identificar recursos
+  * Request Body: 
+  */
+
+app.get('/users', (request, response) => {
+    const params = request.query;
+
+    console.log(params);
+
     return response.json({
         evento: 'Semana Omnistack 11.0',
         aluno: 'Letícia Alves'
